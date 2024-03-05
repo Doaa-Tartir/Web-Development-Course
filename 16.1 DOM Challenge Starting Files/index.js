3959dcbba62dfb33046075d276ac697a84b7b6d7
Doaa Tartir <162336526+Doaa-Tartir@users.noreply.github.com>
2024-03-05 23:12:24 +0200

    Add files via upload

---
+++ 16.1 DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML="Goal Achieved";
+document.querySelector(".list a").style.color="red";
+document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("h1").classList.add("huge");
