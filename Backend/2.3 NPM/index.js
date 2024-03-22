37df7d11cf27656a7c2cd78945789eac6ad2a53e
Doaa Tartir <162336526+Doaa-Tartir@users.noreply.github.com>
2024-03-23 00:57:35 +0200

    Add files via upload

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import generateName from "sillyname";
+var sillyName=generateName();
+console.log("My name is "+ sillyName);
+
+import superheroes from "superheroes";
+var superheroName=superheroes.random();
+console.log("My superhero name is "+ superheroName);
